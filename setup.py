540437a2d9d444669180537ee0e1b132a51fc2de
neda60 <nebrahimi81@gmail.com>
2023-04-26 13:00:38 -0700

    setup added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'mlproject',
+    version='0.0.1',
+    author='Neda',
+    packages=find_packages(),
+    requires='requirements.txt'
+)
