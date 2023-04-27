71073aea4c0498f0af00af08a36d46ec3b8d6f8f
Neda Ebrahimi <nebrahimi81@gmail.com>
2023-04-27 13:24:32 -0700

    Data Ingestion added

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .' # this is for running the setup.py automatically in requirements.py
+
+def get_requirements(file_path: str)-> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readline()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
-    name = 'mlproject',
+    name = 'mlproject_end2end',
     version='0.0.1',
     author='Neda',
     packages=find_packages(),
-    requires='requirements.txt'
+    requires= get_requirements('requirements.txt')
 )
